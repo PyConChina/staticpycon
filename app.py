2972bff2d99fe9bdca442b0b1cbf5d3297cdb621
Todd Gao <todd.gao.2013@gmail.com>
2014-09-27 11:55:27 +0800

    update for english version

--- app.py
+++ app.py
@@ -15,44 +15,46 @@ ASSET_DIR_REl = "asset"
 DATA_DIR = join(PROJECT_DIR, "src", "data")
 
 def mdconv(markdown_source):
-    # md = Markdown(extensions=['meta', 'toc', 'tables', 'fenced_code'])
     md = Markdown()
     html_content = md.convert(markdown_source)
     return html_content
 
 data_mtimes = {}
-pycon_context = {
-    'mdconv' : mdconv
-}
+cn_data_pattern = re.compile("_(\w+)\.cn\.yaml")
+en_data_pattern = re.compile("_(\w+)\.en\.yaml")
+cn_pycon_context = { 'mdconv' : mdconv }
+en_pycon_context = { 'mdconv' : mdconv }
 
-def load_data():
-    global data_mtimes
-    global pycon_context
+def load_data(data_context, data_pattern):
     for filename in listdir(DATA_DIR):
         filepath = join(DATA_DIR, filename)
         filemtime = int(getmtime(filepath))
         if filepath in data_mtimes and filemtime == data_mtimes[filepath]:
             continue # skip unmodified file
         data_mtimes[filepath] = filemtime
-        if "_site.yaml" == filename:
+        match = data_pattern.match(filename)
+        if match:
+            entryname = match.group(1)
             data = yaml.load(open(filepath))
-            pycon_context.update(**data)
-            continue
-        entryname = re.search("_(\w+)\.yaml", filename).group(1)
-        data = yaml.load(open(filepath))
-        pycon_context[entryname] = data
+            data_context[entryname] = data
 
 def render_cn_page(renderer, template, **context):
-    global pycon_context
-    load_data()
+    load_data(cn_pycon_context, cn_data_pattern)
     outfile = template.name.replace(".cn.html", ".html")
     filepath = join(SITE_DIR, outfile)
-    template.stream(pycon_context).dump(filepath, "utf8")
+    template.stream(cn_pycon_context).dump(filepath, "utf8")
+
+def render_en_page(renderer, template, **context):
+    load_data(en_pycon_context, en_data_pattern)
+    outfile = template.name # template.name.replace(".en.html", ".html")
+    filepath = join(SITE_DIR, outfile)
+    template.stream(en_pycon_context).dump(filepath, "utf8")
 
 def dev():
     renderer = make_renderer(searchpath=SOURCE_DIR, staticpath=ASSET_DIR_REl,
         outpath=SITE_DIR, rules=[
             ("[\w-]+\.cn\.html", render_cn_page),
+            ("[\w-]+\.en\.html", render_en_page),
         ])
     def serve():
         from SimpleHTTPServer import SimpleHTTPRequestHandler
