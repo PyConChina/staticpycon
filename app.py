0a1f9e35324263ac282a6097ca4872ef476f48d8
Todd Gao <todd.gao.2013@gmail.com>
2014-10-10 12:25:25 +0800

    更新赞助商logo；修改各页面标题设置；clean code

--- app.py
+++ app.py
@@ -3,32 +3,24 @@
 
 from os.path import realpath, dirname, join, getmtime
 from os.path import exists as file_exists
-from os import listdir, makedirs
+from os import listdir, makedirs, chdir
 from staticjinja import make_renderer
-import yaml, re
+import yaml, re, thread, sys
 from copy import deepcopy
 
 PROJECT_DIR = realpath(dirname(__file__))
 SITE_DIR = join(PROJECT_DIR, "out")
 SOURCE_DIR = join(PROJECT_DIR, "src")
 ASSET_DIR = join(PROJECT_DIR, "src", "asset")
-ASSET_DIR_REl = "asset"
+ASSET_DIR_REL = "asset"
 DATA_DIR = join(PROJECT_DIR, "src", "data")
 
 data_mtimes = {}
-data_table = {
-    'cn' : {
-        'suffix'  : '_cn',
-        'basedir' : "",
-        'context' : { 'lang' : 'cn' }
-    },
-    'en' : {
-        'suffix'  : '_en',
-        'basedir' : 'en',
-        'context' : { 'lang' : 'en' }
-    }
-}
 data_pattern = re.compile("_(\w+)\.yaml")
+data_contexts = {
+    'cn' : { 'lang' : 'cn', 'lang_suffix' : '_cn', 'lang_dir' : '' },
+    'en' : { 'lang' : 'en', 'lang_suffix' : '_en', 'lang_dir' : 'en' },
+}
 
 def process_data(data, suffix):
     if isinstance(data, list):
@@ -43,7 +35,7 @@ def process_data(data, suffix):
                 if kn in data:
                     data[kn] = v
                 else:
-                    print 'Warning: invalid attribute ', kn
+                    print('Warning: invalid attribute %s' % kn)
 
 def load_data():
     for filename in listdir(DATA_DIR):
@@ -56,38 +48,36 @@ def load_data():
             data_mtimes[filepath] = filemtime
             entryname = match.group(1)
             data = yaml.load(open(filepath))
-            for lang, v in data_table.iteritems():
+            for lang, context in data_contexts.items():
                 data_copy = deepcopy(data)
-                process_data(data_copy, v['suffix'])
-                v['context'][entryname] = data_copy
+                process_data(data_copy, context['lang_suffix'])
+                context[entryname] = data_copy
 
 def render_page(renderer, template, **context):
     load_data()
-    for lang, v in data_table.iteritems():
-        outfile = join(SITE_DIR, v['basedir'], template.name)
-        # ensure dir
+    for lang, context in data_contexts.items():
+        outfile = join(SITE_DIR, context['lang_dir'], template.name)
         head = dirname(outfile)
         if head and not file_exists(head):
             makedirs(head)
-        print "Generating [%s] %s ..." % (lang, outfile)
-        template.stream(v['context']).dump(outfile, "utf8") 
+        print("Generating [%s] %s ... " % (context['lang'], outfile))
+        template.stream(context).dump(outfile, "utf-8")
 
-def dev():
-    renderer = make_renderer(searchpath=SOURCE_DIR, staticpath=ASSET_DIR_REl,
+def main():
+    renderer = make_renderer(searchpath=SOURCE_DIR, staticpath=ASSET_DIR_REL,
         outpath=SITE_DIR, rules=[
             ("[\w-]+\.html", render_page)
         ])
     def serve():
         from SimpleHTTPServer import SimpleHTTPRequestHandler
         from SocketServer import TCPServer
-        import os
         print("Listening on 127.0.0.1:8080 ...")
         server = TCPServer(('127.0.0.1', 8080), SimpleHTTPRequestHandler)
-        os.chdir(SITE_DIR)
+        chdir(SITE_DIR)
         server.serve_forever()
-    import thread
-    thread.start_new_thread(serve, ())
-    renderer.run(use_reloader=True)
+    dev_mode = (not "-g" in sys.argv)
+    if dev_mode: thread.start_new_thread(serve, ())
+    renderer.run(use_reloader=dev_mode)
 
 if __name__ == "__main__":
-    dev()
+    main()
