d11e2f8d44c4960f4408beaf75354b5f4e89859d
Wang Xuerui <idontknw.wang@gmail.com>
2014-11-12 21:46:53 +0800

    bin: 设置 pyScss log 输出

--- bin/staticpycon/util.py
+++ bin/staticpycon/util.py
@@ -5,6 +5,9 @@ from __future__ import unicode_literals, absolute_import
 
 import os
 import errno
+import logging
+
+from ._colorama_compat import colorama
 
 
 # http://stackoverflow.com/a/600612/596531
@@ -18,6 +21,42 @@ def mkdirp(path):
             pass
         else:
             raise
+
+
+def setup_logging():
+    # scss compiler output
+    logging.getLogger('scss.compiler').addHandler(SCSSMessagesHandler())
+
+
+# formatter for pyScss messages
+class SCSSMessagesFormatter(logging.Formatter):
+    '''Formatter for pyScss outputs.'''
+
+    FMT_WARNING = (
+        colorama.Style.DIM
+        + colorama.Fore.CYAN
+        + 'scss '
+        + colorama.Fore.YELLOW
+        + 'warn '
+        + colorama.Style.RESET_ALL
+        + '%s'
+    )
+
+    def format(self, record):
+        if record.levelno == logging.WARNING:
+            return self.FMT_WARNING % (record.msg, )
+
+        return super(SCSSMessagesFormatter, self).format(record)
+
+
+class SCSSMessagesHandler(logging.StreamHandler):
+    '''Logging handler for pyScss.'''
+
+    def __init__(self, *args, **kwargs):
+        super(SCSSMessagesHandler, self).__init__(*args, **kwargs)
+
+        # use custom formatter
+        self.setFormatter(SCSSMessagesFormatter())
 
 
 # vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
